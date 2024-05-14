dee5518290da90cd598a64c64dec5599950ed5cd
Ilaria Nuzzaco <ilarianuzzaco@live.it>
2024-05-14 22:56:54 +0200

    fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ var submitError = document.getElementById('submit-error');
 function validateName(){
   var name = document.getElementById('contact-name').value;
 
-  if(name.lenght == 0){
+  if(name.length == 0){
     nameError.innerHTML = 'Name is required';
     return false;
   }
@@ -22,23 +22,23 @@ function validateName(){
 function validatePhone(){
     var phone = document.getElementById('contact-phone').value;
 
-if(phone.lenght == 0){
-    phoneError.innerHTML = 'Phone no. is required';
-    return false;
-}
-if(!phone.match(/^[0-9]{10}$/)){
-    phoneError.innerHTML = 'Phone no. should be 10 digits';
-    return false;
-}
+    if(phone.length == 0){
+        phoneError.innerHTML = 'Phone no. is required';
+        return false;
+    }
+    if(!phone.match(/^[0-9]{10}$/)){
+        phoneError.innerHTML = 'Phone no. should be 10 digits';
+        return false;
+    }
 
-phoneError.innerHTML = '<i class="fa fa-check-circle"></i>';
-return true;
+    phoneError.innerHTML = '<i class="fa fa-check-circle"></i>';
+    return true;
 }
 
 function validateEmail(){
   var email = document.getElementById('contact-email').value;
 
-  if(email.lenght == 0){
+  if(email.length == 0){
     emailError.innerHTML = 'Email is required'
     return false;
   }
@@ -48,7 +48,7 @@ function validateEmail(){
   }
 
   emailError.innerHTML = '<i class="fa fa-check-circle"></i>';
-return true;
+  return true;
 }
 
 function validateMessage(){
@@ -61,18 +61,20 @@ function validateMessage(){
     return false;
   }
 
-messageError.innerHTML = '<i class="fa fa-check-circle"></i>';
-return true;
+  messageError.innerHTML = '<i class="fa fa-check-circle"></i>';
+  return true;
 }
 
+
 function validateForm() {
-  if(!validateName() || !validatePhone() || !validateEmail() || !validateMessage()){
+  if (!validateName() || !validatePhone() || !validateEmail() || !validateMessage()) {
     submitError.style.display = 'block';
-    submitError.innerHTML = 'Please fix error to submit';
-    setTimeout(function(){submitError.style.display = 'none';}, 3000);
+    submitError.innerHTML = 'Please fix errors to submit';
+    setTimeout(function() {
+      submitError.style.display = 'none';
+    }, 3000);
     return false;
+  } else {
+    return 'Successful request!';
   }
-  else(
-    resolve( 'Successfull request!' )
-  )
 }
